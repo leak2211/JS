c90d47b243037b9b28d3dfb269bba29e89171e3e
leak2211 <144492917+leak2211@users.noreply.github.com>
2025-01-15 11:00:10 +0300

    Update race.js

--- hw,5/race.js
+++ hw,5/race.js
@@ -68,7 +68,7 @@ function destroy() {
   let labels = this.data.getColumn(0);
 
   // Colour to use for each category.
-  let colours = ['pink', 'blue', 'red', 'green', 'purple'];
+  let colours = ['pink', 'blue', 'red', 'green', 'purple', 'black'];
 
   // Make a title.
   let title = question;
